65ce8d0a7caca21206fb7ad2c9d55b199d44417c
Carlos-asus <charles.roblesp@gmail.com>
2016-12-26 17:55:48 +0100

    .....

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -34,7 +34,7 @@ WARNING!!! WARNING!!!
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 
-(function(window) {
+(function() {
 
 	var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
@@ -54,29 +54,27 @@ WARNING!!! WARNING!!!
   // to lower case character 'j' afterwards.
   // Look up these methods on Mozilla Developer Network web site if needed.
   
-  		var firstLetter = names[i].charAt(0).toLowerCase();
+		var firstLetter = names[i].charAt(0).toLowerCase();
 
   // STEP 12:
   // Compare the 'firstLetter' retrieved in STEP 11 to lower case
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-	    if ( /* fill in condition here */ firstLetter === "j"){
+		if ( /* fill in condition here */ firstLetter === "j"){
 	    
-	   		byeSpeaker.speak(names[i]);
+			byeSpeaker.speak(names[i]);
 	   		
 		// byeSpeaker.xxxx
-	    } else {
+		} else {
 	    
-	    	helloSpeaker.speak(names[i]);
+			helloSpeaker.speak(names[i]);
 	    
 		// helloSpeaker.xxxx
-	    }
+		}
 	}
 	
-	window.names = names
-	
-})(window);
+})();
 
 
 
